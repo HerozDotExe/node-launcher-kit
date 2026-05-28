a09bec73a6c5dc5c1e00455b26c8d2c2be938891
HerozDotExe <herozdotexe@gmail.com>
2026-05-28 18:09:27 +0200

    fix: remove unneeded js banner that make the commonjs build unusable

--- tsup.config.js
+++ tsup.config.js
@@ -4,8 +4,5 @@ export default defineConfig({
   entry: ["src/index.ts"],
   target: "node22",
   format: ["cjs", "esm"],
-  dts: true,
-  banner: {
-    js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);",
-  },
+  dts: true
 });
